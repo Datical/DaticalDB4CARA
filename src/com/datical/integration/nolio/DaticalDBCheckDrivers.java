430635529bc81241845fb871b2c51d0e853a9a81
r2datical <r2@datical.com>
2014-04-14 11:39:57 -0500

    Added more actions

--- src/com/datical/integration/nolio/DaticalDBCheckDrivers.java
+++ src/com/datical/integration/nolio/DaticalDBCheckDrivers.java
@@ -1,81 +1,19 @@
 package com.datical.integration.nolio;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import com.nolio.platform.shared.api.ActionDescriptor;
+import com.nolio.platform.shared.api.NolioAction;
 
-import com.nolio.platform.shared.api.*;
+@ActionDescriptor(
+		name = "Datical DB Check Drivers Action", 
+		description = "This action verified that Datical DB can access the correct database drivers.", 
+		category = "Datical.Check Drivers")
 
-/**
- * An example Nolio action
- * 
- * <p>
- * Date: Apr 10, 2014
- * </p>
- * 
- * @author robert
- */
-@ActionDescriptor(name = "Datical DB Check Drivers Action", description = "This action verified that Datical DB can access the correct database drivers.", category = "Datical.Check Drivers")
-public class DaticalDBCheckDrivers implements NolioAction {
-	private static final long serialVersionUID = 1L;
-
-	@ParameterDescriptor(
-			name = "Datical DB Location", 
-			description = "Fully qualified path to Datical DB CLI installation (e.g., C:\\DaticalDB\\repl\\hammer.bat or /usr/local/bin/DaticalDB/repl/hammer)", 
-			out = false, 
-			in = true, 
-			nullable = false,
-			defaultValueAsString = "C:\\DaticalDB-1.32.438\\repl\\hammer.bat", 
-			order = 1)
-	private String daticalDBLocation = "C:\\DaticalDB-1.32.438\\repl\\hammer.bat";
-
-	@ParameterDescriptor(
-			name = "Datical DB Project Directory", 
-			description = "The Datical DB Project Directory is the path location of your Datical DB Project folder. This will contain a datical.project file.", 
-			out = false, 
-			in = true, 
-			nullable = false, 
-			defaultValueAsString = "C:\\Users\\robert\\datical\\BigProject", 
-			order = 2)
-	private String daticalDBProjectDirectory = "C:\\Users\\robert\\datical\\BigProject";
-
+public class DaticalDBCheckDrivers extends DaticalDBActionWithoutTarget implements NolioAction {
 	
-    @ParameterDescriptor(
-        name = "Datical DB Output",
-        description = "Output of the Datical DB Command",
-        out = true,
-        in = false)
-    private String daticalDBOutput;	
-	
-	
-	@Override
-	public ActionResult executeAction() {
-
+	private static final long serialVersionUID = -2353494281884924346L;
 		
-		
-		_log.warn("My custom log message");
-		
-
-		
-		String output = "";
-		try {
-			_log.info("Starting Datical DB.");
-			Process p = new ProcessBuilder(daticalDBLocation, "--project", daticalDBProjectDirectory, "checkdrivers").start();
-			_log.info("Waiting for Datical DB to complete.");
-			p.waitFor();
-			_log.info("Datical DB completed.");
-			BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-			while ((output = b.readLine()) != null) {
-				daticalDBOutput = daticalDBOutput + "\n" + output;
-			}
-
-		} catch (IOException | InterruptedException e) {
-			_log.error(e.toString());
-		}
-
-		_log.info(daticalDBOutput);
-		return new ActionResult(true, daticalDBOutput);
-
+	public DaticalDBCheckDrivers() {
+		super();
+		setDaticalDBAction("checkdrivers");
 	}
 }
